58ffc6bf2a2be4b34a2e20cdd507183541ac0e0d
Xamoscivo <96232500+Xamoscivo@users.noreply.github.com>
2021-12-21 13:27:41 +0700

    Create 300.js

---
+++ 300.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'bcd184538fd9c67cc111078ddf2686c6',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://peruseinvitation.com/bcd184538fd9c67cc111078ddf2686c6/invoke.js"></scr' + 'ipt>');
